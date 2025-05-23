c12c2cd8fae5563bacef246c4e88489665a36863
UPC-Skylar <zaynml2021@gmail.com>
2025-05-22 22:45:13 -0500

    feat(i18n): add language switcher component.

---
+++ src/public/components/language-switcher.component.vue
@@ -0,0 +1,21 @@
+<script>
+import {Button as PvSelectButton} from "primevue";
+
+export default {
+  name: "language-switcher",
+  components: {PvSelectButton},
+  data(){
+    return {
+      languages: ['en', 'es'],
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-select-button v-model="$i18n.locale" :options="languages" class="uppercase"/>
+</template>
+
+<style scoped>
+
+</style>
